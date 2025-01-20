66183e5b30caf5fafe9309acba5beba7213847b7
Christopher Tamayo <chriswtamayo@gmail.com>
2025-01-20 11:38:22 -0800

    Add about section

--- client/src/views/AboutPage/AboutPage.jsx
+++ client/src/views/AboutPage/AboutPage.jsx
@@ -12,8 +12,13 @@ const AboutPage = () => {
         </div>
         <hr/>
 
-        <div className="ingredients-container">
-          About
+        <div>
+          <p>
+            Healthy Steps Hawai'i is an O'ahu-based 501(c)(3) nonprofit organization formed in 2023 aimed at helping the people of Hawai'i take the first steps towards a healthier future. We are interested in the social determinants of health and believe that by combating these problems, we can help guide Hawai'i toward a healthier future. 
+          </p>
+          <p>
+            Please use this web based application to help you create delicious, healthy, and affordable recipes for you and your family to enjoy!
+          </p>
         </div>
         
       </div>
